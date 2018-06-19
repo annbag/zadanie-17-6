b4e8ee2708d78b77092777c8c00808da0dfed24a
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-06-20 00:07:17 +0200

    add part task

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
 var express = require('express');
 var app = express();
+
 app.set('view engine', 'pug');
 app.set('views','./views');
+
+
+app.get('/', function (req, res) {
+	res.render('login', {
+		url: "http://localhost:4000/logged"
+	})
+})
+
+app.get('/logged', function (req, res) {
+	res.render('logged')
+})
+
+app.listen(4000);
+app.use(function (req, res, next) {
+    res.status(404).send('Page not found!')
+});
